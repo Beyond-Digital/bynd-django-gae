07a06b5d895a701b399b339f6936544125c052a7
George Whewell <george@bynd.com>
2013-08-09 09:46:33 +0100

    initial commit

---
+++ appengine_config.py
@@ -0,0 +1,27 @@
+import os
+
+"""
+Namespacing:
+
+We split version id on "-" and use the first segment as namespace
+
+Hence unique namespaces are:
+
+local
+dev
+qa
+staging-*
+live-*
+
+Applies to datastore, memcache and task queues
+See: https://developers.google.com/appengine/docs/python/multitenancy/multitenancy#Setting_the_Current_Namespace
+"""
+try:
+    namespace = os.environ['CURRENT_VERSION_ID'].split('.')[0].split('-')[0]
+except KeyError:
+    # This means we're running from manage.py
+    namespace = os.environ.get('DJANGO_ENV', 'local')
+
+
+def namespace_manager_default_namespace_for_request():
+    return namespace
---
+++ manage.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import os
+from subprocess import check_output, Popen, PIPE
+import sys
+
+# Add GAE libs to path
+try:
+    appserver_location = check_output("which dev_appserver.py", shell=True).strip()
+    appserver_path = os.path.split(os.path.realpath(appserver_location))[0]
+    sys.path.append(appserver_path)
+except:
+    print "Cant find appserver, probably going to fail"
+    pass
+
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
